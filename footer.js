2ef2bfa7aa5a86d6d1ac37db0813854b42d3d644
LuckyJQ <shenjianqi1111@126.com>
2020-07-18 19:52:53 +0800

    初始化webpack场景，最简单的打包例子

---
+++ footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    var dom = document.getElementById('root');
+    var footer = document.createElement('div');
+    footer.innerText = 'footer';
+    dom.appendChild(footer);
+}
+
+export default Footer;
